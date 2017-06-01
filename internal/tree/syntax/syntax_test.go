010ff055344bc47927d5619045d92e896360a5a4
caixw <xnotepad@gmail.com>
2017-06-01 22:08:32 +0800

    [internal/tree/syntax] 删除 Check 函数
    Parse 已经兼有此函数功能，不再需要 Check 函数

--- internal/tree/syntax/syntax_test.go
+++ internal/tree/syntax/syntax_test.go
@@ -59,30 +59,3 @@ func TestPrefixLen(t *testing.T) {
 	a.Equal(PrefixLen("/tes{t}", "/tes{t}"), 7)
 	a.Equal(PrefixLen("/tes{t:\\d+}", "/tes{t:\\d+}"), 4)
 }
-
-func TestCheck(t *testing.T) {
-	a := assert.New(t)
-
-	test := func(pattern string, nType Type, isError bool) {
-		typ, err := Check(pattern)
-		if isError {
-			a.Error(err)
-			return
-		}
-
-		a.NotError(err).Equal(nType, typ)
-	}
-
-	test("{{", TypeUnknown, true)
-	test("}{", TypeUnknown, true)
-	test("{}:", TypeUnknown, true)
-	test("{}", TypeUnknown, true)
-	test("{:}", TypeUnknown, true)
-	test("{a:}:", TypeUnknown, true)
-
-	test("{a:}", TypeRegexp, false)
-	test("{a:\\d+}", TypeRegexp, false)
-	test("{a}", TypeNamed, false)
-	test("{a}/*", TypeWildcard, false)
-	test("{a:\\d+}/*", TypeWildcard, false)
-}
