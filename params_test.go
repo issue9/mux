cfebd4558bf571a77a8a8ac2df59eed88d64ac38
caixw <xnotepad@gmail.com>
2017-06-11 23:09:55 +0800

    添加测试内容

--- params_test.go
+++ params_test.go
@@ -38,6 +38,25 @@ func TestGetParams(t *testing.T) {
 	a.Equal(ps, maps)
 }
 
+func TestParams_String(t *testing.T) {
+	a := assert.New(t)
+
+	ps := getParams(map[string]string{
+		"key1": "1",
+	}, a)
+
+	val, err := ps.String("key1")
+	a.NotError(err).Equal(val, "1")
+	a.True(ps.Exists("key1"))
+	a.Equal(ps.MustString("key1", "-9"), "1")
+
+	// 不存在
+	val, err = ps.String("k5")
+	a.ErrorType(err, ErrParamNotExists).Equal(val, "")
+	a.False(ps.Exists("k5"))
+	a.Equal(ps.MustString("k5", "-10"), "-10")
+}
+
 func TestParams_Int(t *testing.T) {
 	a := assert.New(t)
 
@@ -59,6 +78,35 @@ func TestParams_Int(t *testing.T) {
 	val, err = ps.Int("k5")
 	a.ErrorType(err, ErrParamNotExists).Equal(val, 0)
 	a.Equal(ps.MustInt("k5", -10), -10)
+}
+
+func TestParams_Uint(t *testing.T) {
+	a := assert.New(t)
+
+	ps := getParams(map[string]string{
+		"key1": "1",
+		"key2": "a2",
+		"key3": "-1",
+	}, a)
+
+	val, err := ps.Uint("key1")
+	a.NotError(err).Equal(val, 1)
+	a.Equal(ps.MustUint("key1", 9), 1)
+
+	// 无法转换
+	val, err = ps.Uint("key2")
+	a.Error(err).Equal(val, 0)
+	a.Equal(ps.MustUint("key2", 9), 9)
+
+	// 负数
+	val, err = ps.Uint("key3")
+	a.Error(err).Equal(val, 0)
+	a.Equal(ps.MustUint("key3", 9), 9)
+
+	// 不存在
+	val, err = ps.Uint("k5")
+	a.ErrorType(err, ErrParamNotExists).Equal(val, 0)
+	a.Equal(ps.MustUint("k5", 10), 10)
 }
 
 func TestParams_Bool(t *testing.T) {
@@ -87,4 +135,32 @@ func TestParams_Bool(t *testing.T) {
 	val, err = ps.Bool("k5")
 	a.ErrorType(err, ErrParamNotExists).False(val)
 	a.True(ps.MustBool("k5", true))
+}
+
+func TestParams_Float(t *testing.T) {
+	a := assert.New(t)
+
+	ps := getParams(map[string]string{
+		"key1": "1",
+		"key2": "a2",
+		"key3": "1.1",
+	}, a)
+
+	val, err := ps.Float("key1")
+	a.NotError(err).Equal(val, 1.0)
+	a.Equal(ps.MustFloat("key1", -9.0), 1.0)
+
+	val, err = ps.Float("key3")
+	a.NotError(err).Equal(val, 1.1)
+	a.Equal(ps.MustFloat("key3", -9.0), 1.1)
+
+	// 无法转换
+	val, err = ps.Float("key2")
+	a.Error(err).Equal(val, 0.0)
+	a.Equal(ps.MustFloat("key2", -9.0), -9.0)
+
+	// 不存在
+	val, err = ps.Float("k5")
+	a.ErrorType(err, ErrParamNotExists).Equal(val, 0.0)
+	a.Equal(ps.MustFloat("k5", -10.0), -10.0)
 }
