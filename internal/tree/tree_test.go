19f4060367cf066f3ca24ee2b0b431a6f005d36d
caixw <xnotepad@gmail.com>
2017-07-03 00:28:37 +0800

    [internal/tree] 调整测试代码
    将部分 Node 上的测试代码移到 Tree 上

--- internal/tree/tree_test.go
+++ internal/tree/tree_test.go
@@ -6,15 +6,205 @@ package tree
 
 import (
 	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/issue9/assert"
+	"github.com/issue9/mux/internal/tree/handlers"
+	"github.com/issue9/mux/internal/tree/segment"
 )
 
 func buildHandler(code int) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(code)
 	})
+}
+
+func split(a *assert.Assertion, pattern string) []string {
+	ss, err := segment.Split(pattern)
+	a.NotError(err).NotNil(ss)
+	return ss
+}
+
+type tester struct {
+	n *Tree
+	a *assert.Assertion
+}
+
+func newTester(a *assert.Assertion) *tester {
+	seg, err := segment.New("")
+	a.NotError(err).NotNil(seg)
+
+	return &tester{
+		n: New(),
+		a: a,
+	}
+}
+
+// 添加一条路由项。code 表示该路由项返回的报头，
+// 测试路由项的 code 需要唯一，之后也是通过此值来判断其命中的路由项。
+func (n *tester) add(method, pattern string, code int) {
+	nn, err := n.n.getNode(split(n.a, pattern))
+	n.a.NotError(err).NotNil(nn)
+
+	if nn.handlers == nil {
+		nn.handlers = handlers.New()
+	}
+
+	nn.handlers.Add(buildHandler(code), method)
+}
+
+// 验证指定的路径是否匹配正确的路由项，通过 code 来确定，并返回该节点的实例。
+func (n *tester) matchTrue(method, path string, code int) *Node {
+	nn := n.n.match(path)
+	n.a.NotNil(nn)
+
+	h := nn.Handler(method)
+	n.a.NotNil(h)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(method, path, nil)
+	h.ServeHTTP(w, r)
+	n.a.Equal(w.Code, code)
+
+	return nn
+}
+
+// 验证指定的路径返回的参数是否正确
+func (n *tester) paramsTrue(method, path string, code int, params map[string]string) {
+	nn := n.matchTrue(method, path, code)
+
+	ps := nn.Params(path)
+	n.a.Equal(ps, params)
+}
+
+// 验证 Node.URL 的正确性
+// method+path 用于获取指定的节点
+func (n *tester) urlTrue(method, path string, code int, params map[string]string, url string) {
+	nn := n.matchTrue(method, path, code)
+
+	u, err := nn.URL(params)
+	n.a.NotError(err)
+	n.a.Equal(u, url)
+}
+
+func TestTree_match(t *testing.T) {
+	a := assert.New(t)
+	test := newTester(a)
+
+	// 添加路由项
+	test.add(http.MethodGet, "/", 1)
+	test.add(http.MethodGet, "/posts/{id}", 2)
+	test.add(http.MethodGet, "/posts/{id}/author", 3)
+	test.add(http.MethodGet, "/posts/1/author", 4)
+	test.add(http.MethodGet, "/posts/{id:\\d+}", 5)
+	test.add(http.MethodGet, "/posts/{id:\\d+}/author", 6)
+	test.add(http.MethodGet, "/page/{page:\\d*}", 7) // 可选的正则节点
+	test.add(http.MethodGet, "/posts/{id}/{page}/author", 8)
+	//test.n.print(0)
+
+	test.matchTrue(http.MethodGet, "/", 1)
+	test.matchTrue(http.MethodGet, "/posts/1", 5)             // 正则
+	test.matchTrue(http.MethodGet, "/posts/2", 5)             // 正则
+	test.matchTrue(http.MethodGet, "/posts/2/author", 6)      // 正则
+	test.matchTrue(http.MethodGet, "/posts/1/author", 4)      // 字符串
+	test.matchTrue(http.MethodGet, "/posts/1.html", 2)        // 命名参数
+	test.matchTrue(http.MethodGet, "/posts/1.html/page", 2)   // 命名参数
+	test.matchTrue(http.MethodGet, "/posts/2.html/author", 3) // 命名参数
+	test.matchTrue(http.MethodGet, "/page/", 7)
+	test.matchTrue(http.MethodGet, "/posts/2.html/2/author", 8) // 若 {id} 可匹配任意字符，此条也可匹配 3
+
+	// 以斜框结尾，是否能正常访问
+	test = newTester(a)
+	test.add(http.MethodGet, "/posts/{id}/", 1)
+	test.add(http.MethodGet, "/posts/{id}/author", 2)
+	test.matchTrue(http.MethodGet, "/posts/1/", 1)
+	test.matchTrue(http.MethodGet, "/posts/1.html/", 1)
+	test.matchTrue(http.MethodGet, "/posts/1/author", 2)
+
+	// 以 - 作为路径分隔符
+	test = newTester(a)
+	test.add(http.MethodGet, "/posts-{id}", 1)
+	test.add(http.MethodGet, "/posts-{id}-author", 2)
+	test.matchTrue(http.MethodGet, "/posts-1.html", 1)
+	test.matchTrue(http.MethodGet, "/posts-1-author", 2)
+
+	test = newTester(a)
+	test.add(http.MethodGet, "/admin/{path}", 1)
+	test.add(http.MethodGet, "/admin/items/{id:\\d+}", 2)
+	test.add(http.MethodGet, "/admin/items/{id:\\d+}/profile", 3)
+	test.add(http.MethodGet, "/admin/items/{id:\\d+}/profile/{type:\\d+}", 4)
+	test.matchTrue(http.MethodGet, "/admin/index.html", 1)
+	test.matchTrue(http.MethodGet, "/admin/items/1", 2)
+	test.matchTrue(http.MethodGet, "/admin/items/1/profile", 3)
+	test.matchTrue(http.MethodGet, "/admin/items/1/profile/1", 4)
+	test.n.trace(os.Stdout, 0, "/admin/items/1/profile/1")
+}
+
+func TestTree_Params(t *testing.T) {
+	a := assert.New(t)
+	test := newTester(a)
+
+	// 添加路由项
+	test.add(http.MethodGet, "/posts/{id}", 1)                       // 命名
+	test.add(http.MethodGet, "/posts/{id}/author/{action}/", 2)      // 命名
+	test.add(http.MethodGet, "/posts/{id:\\d+}", 3)                  // 正则
+	test.add(http.MethodGet, "/posts/{id:\\d+}/author/{action}/", 4) // 正则
+
+	// 正则
+	test.paramsTrue(http.MethodGet, "/posts/1", 3, map[string]string{"id": "1"})
+	test.paramsTrue(http.MethodGet, "/posts/1/author/profile/", 4, map[string]string{"id": "1", "action": "profile"})
+
+	// 命名
+	test.paramsTrue(http.MethodGet, "/posts/1.html", 1, map[string]string{"id": "1.html"})
+	test.paramsTrue(http.MethodGet, "/posts/1.html/author/profile/", 2, map[string]string{"id": "1.html", "action": "profile"})
+}
+
+func TestTree_URL(t *testing.T) {
+	a := assert.New(t)
+	test := newTester(a)
+
+	// 添加路由项
+	test.add(http.MethodGet, "/posts/{id}", 1)                       // 命名
+	test.add(http.MethodGet, "/posts/{id}/author/{action}/", 2)      // 命名
+	test.add(http.MethodGet, "/posts/{id:\\d+}", 3)                  // 正则
+	test.add(http.MethodGet, "/posts/{id:\\d+}/author/{action}/", 4) // 正则
+
+	test.urlTrue(http.MethodGet, "/posts/1", 3, map[string]string{"id": "100"}, "/posts/100")
+	test.urlTrue(http.MethodGet, "/posts/1/author/profile/", 4, map[string]string{"id": "100", "action": "p"}, "/posts/100/author/p/")
+	test.urlTrue(http.MethodGet, "/posts/1.html", 1, map[string]string{"id": "100.htm"}, "/posts/100.htm")
+	test.urlTrue(http.MethodGet, "/posts/1.html/author/profile/", 2, map[string]string{"id": "100.htm", "action": "p"}, "/posts/100.htm/author/p/")
+}
+
+func TestTree_Clean(t *testing.T) {
+	a := assert.New(t)
+	tree := New()
+
+	addNode := func(p string, code int, methods ...string) {
+		nn, err := tree.GetNode(p)
+		a.NotError(err).NotNil(nn)
+
+		if nn.handlers == nil {
+			nn.handlers = handlers.New()
+		}
+
+		a.NotError(nn.handlers.Add(buildHandler(code), methods...))
+	}
+
+	addNode("/", 1, http.MethodGet)
+	addNode("/posts/1/author", 1, http.MethodGet)
+	addNode("/posts/{id}", 1, http.MethodGet)
+	addNode("/posts/{id}/author", 1, http.MethodGet)
+	addNode("/posts/{id}/{author:\\w+}/profile", 1, http.MethodGet)
+
+	a.Equal(tree.len(), 5)
+
+	tree.Clean("/posts/{id")
+	a.Equal(tree.len(), 2)
+
+	tree.Clean("")
+	a.Equal(tree.len(), 0)
 }
 
 func TestTree_Add_Remove(t *testing.T) {
