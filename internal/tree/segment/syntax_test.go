4002a015098da0e9e764060daa7dc9a32720d4da
caixw <xnotepad@gmail.com>
2017-07-04 23:42:02 +0800

    删除不必要的文件和函数

---
+++ internal/tree/segment/syntax_test.go
@@ -0,0 +1,39 @@
+// Copyright 2017 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package segment
+
+import (
+	"testing"
+
+	"github.com/issue9/assert"
+)
+
+func TestLongestPrefix(t *testing.T) {
+	a := assert.New(t)
+
+	test := func(s1, s2 string, len int) {
+		a.Equal(LongestPrefix(s1, s2), len)
+	}
+
+	test("", "", 0)
+	test("/", "", 0)
+	test("/test", "test", 0)
+	test("/test", "/abc", 1)
+	test("/test", "/test", 5)
+	test("/te{st}", "/test", 3)
+	test("/test", "/tes{t}", 4)
+	test("/tes{t:\\d+}", "/tes{t:\\d+}/a", 4) // 不应该包含正则部分
+	test("/tes{t:\\d+}/a", "/tes{t:\\d+}/", 12)
+	test("{t}/a", "{t}/b", 4)
+	test("{t}/abc", "{t}/bbc", 4)
+	test("/tes{t:\\d+}", "/tes{t}", 4)
+}
+
+func TestRepl(t *testing.T) {
+	a := assert.New(t)
+
+	a.Equal(repl.Replace("{id:\\d+}"), "(?P<id>\\d+)")
+	a.Equal(repl.Replace("{id:\\d+}/author"), "(?P<id>\\d+)/author")
+}
