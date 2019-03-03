aaaae4a1c75ce0c35c1d5feb3be02d49c825828f
caixw <xnotepad@gmail.com>
2019-03-03 17:32:35 +0800

    [internal/syntax] 调整文件结构

--- internal/syntax/syntax_test.go
+++ internal/syntax/syntax_test.go
@@ -37,3 +37,60 @@ func TestType_String(t *testing.T) {
 		_ = (Type(5)).String()
 	})
 }
+
+func TestSplit(t *testing.T) {
+	a := assert.New(t)
+	test := func(str string, isError bool, ss ...*Segment) {
+		if isError {
+			a.Panic(func() {
+				Split(str)
+			})
+			return
+		}
+
+		s := Split(str)
+		a.Equal(len(s), len(ss))
+		for index, seg := range ss {
+			a.Equal(seg, s[index])
+		}
+	}
+
+	test("/", false, NewSegment("/"))
+
+	test("/posts/1", false, NewSegment("/posts/1"))
+
+	test("{action}/1", false, NewSegment("{action}/1"))
+
+	// 以命名参数开头的
+	test("/{action}", false, NewSegment("/"), NewSegment("{action}"))
+
+	// 以通配符结尾
+	test("/posts/{id}", false, NewSegment("/posts/"), NewSegment("{id}"))
+
+	test("/posts/{id}/author/profile", false, NewSegment("/posts/"), NewSegment("{id}/author/profile"))
+
+	// 以命名参数结尾的
+	test("/posts/{id}/author", false, NewSegment("/posts/"), NewSegment("{id}/author"))
+
+	// 命名参数及通配符
+	test("/posts/{id}/page/{page}", false, NewSegment("/posts/"), NewSegment("{id}/page/"), NewSegment("{page}"))
+
+	// 正则
+	test("/posts/{id:\\d+}", false, NewSegment("/posts/"), NewSegment("{id:\\d+}"))
+
+	// 正则，命名参数
+	test("/posts/{id:\\d+}/page/{page}", false, NewSegment("/posts/"), NewSegment("{id:\\d+}/page/"), NewSegment("{page}"))
+
+	test("/posts/{id:}", true)
+	test("/posts/{{id:\\d+}/author", true)
+	test("/posts/{:\\d+}/author", true)
+	test("/posts/{}/author", true)
+	test("/posts/{id}{page}/", true)
+	test("/posts/:id/author", true)
+	test("/posts/{id}/{author", true)
+	test("/posts/}/author", true)
+
+	a.Panic(func() {
+		Split("")
+	})
+}
