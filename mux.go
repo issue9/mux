9c3a921974fedaa695e9f48683c7bbcf2ec63b4c
caixw <xnotepad@gmail.com>
2021-05-03 10:20:16 +0800

    chore: 合并文件

--- mux.go
+++ mux.go
@@ -9,6 +9,8 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/issue9/sliceutil"
+
 	"github.com/issue9/mux/v3/group"
 	"github.com/issue9/mux/v3/internal/handlers"
 	"github.com/issue9/mux/v3/internal/syntax"
@@ -62,7 +64,7 @@ type Router struct {
 	Routes map[string][]string // 键名为请求地址，键值为请求方法
 }
 
-// Default New 的默认参数版本要
+// Default New 的默认参数版本
 func Default() *Mux { return New(false, false, false, nil, nil) }
 
 // New 声明一个新的 Mux
@@ -269,6 +271,40 @@ func (mux *Mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	h.ServeHTTP(w, r)
+}
+
+// New 添加子路由组
+//
+// 该路由只有符合 group.Matcher 的要求才会进入，其它与 Mux 功能相同。
+//
+// name 表示该路由组的名称，需要唯一，否则返回 false；
+func (mux *Mux) New(name string, matcher group.Matcher) (*Mux, bool) {
+	if mux.routers == nil {
+		mux.routers = make([]*Mux, 0, 5)
+	}
+
+	if sliceutil.Count(mux.routers, func(i int) bool { return mux.routers[i].name == name }) > 0 {
+		return nil, false
+	}
+
+	m := New(mux.disableOptions, mux.disableHead, mux.skipCleanPath, mux.notFound, mux.methodNotAllowed)
+	m.name = name
+	m.matcher = matcher
+	mux.routers = append(mux.routers, m)
+	return m, true
+}
+
+func (mux *Mux) match(r *http.Request) (*handlers.Handlers, params.Params) {
+	for _, m := range mux.routers {
+		if hs, ps := m.match(r); hs != nil {
+			return hs, ps
+		}
+	}
+
+	if mux.matcher == nil || mux.matcher.Match(r) {
+		return mux.tree.Handler(r.URL.Path)
+	}
+	return nil, nil
 }
 
 // Name 为一条路由项命名
--- mux_test.go
+++ mux_test.go
@@ -389,6 +389,110 @@ func TestMux_ServeHTTP_Order(t *testing.T) {
 	test.matchTrue(http.MethodGet, "/tags.html", 202)               // f2
 }
 
+func TestMux_New(t *testing.T) {
+	a := assert.New(t)
+
+	m := Default()
+	r, ok := m.New("host", group.NewHosts())
+	a.True(ok).NotNil(r)
+	a.Equal(r.name, "host").Equal(r.disableHead, m.disableHead)
+
+	r, ok = m.New("host", group.NewHosts())
+	a.False(ok).Nil(r)
+
+	r, ok = m.New("host-2", group.NewHosts())
+	a.True(ok).NotNil(r)
+	a.Equal(r.name, "host-2").Equal(r.disableHead, m.disableHead)
+}
+
+// 带域名的路由项
+func TestHosts(t *testing.T) {
+	a := assert.New(t)
+
+	m := Default()
+	router, ok := m.New("host", group.NewHosts("localhost"))
+	a.True(ok).NotNil(router)
+	w := httptest.NewRecorder()
+	router.Get("/t1", buildHandler(201))
+	r := httptest.NewRequest(http.MethodGet, "/t1", nil)
+	m.ServeHTTP(w, r)
+	a.Equal(w.Result().StatusCode, 404)
+
+	w = httptest.NewRecorder()
+	r = httptest.NewRequest(http.MethodGet, "https://localhost/t1", nil)
+	m.ServeHTTP(w, r)
+	a.Equal(w.Result().StatusCode, 201)
+
+	w = httptest.NewRecorder()
+	r = httptest.NewRequest(http.MethodGet, "https://localhost/t1", nil)
+	router.ServeHTTP(w, r) // 由 h 直接访问
+	a.Equal(w.Result().StatusCode, 201)
+
+	w = httptest.NewRecorder()
+	r = httptest.NewRequest(http.MethodGet, "/t1", nil)
+	router.ServeHTTP(w, r) // 由 h 直接访问
+	a.Equal(w.Result().StatusCode, 404)
+
+	// resource
+	m = Default()
+	router, ok = m.New("host", group.NewHosts("localhost"))
+	a.True(ok).NotNil(router)
+	res := router.Resource("/r1")
+	res.Get(buildHandler(202))
+	w = httptest.NewRecorder()
+	r = httptest.NewRequest(http.MethodGet, "/r1", nil)
+	m.ServeHTTP(w, r)
+	a.Equal(w.Result().StatusCode, 404)
+
+	w = httptest.NewRecorder()
+	r = httptest.NewRequest(http.MethodGet, "http://localhost/r1", nil)
+	m.ServeHTTP(w, r)
+	a.Equal(w.Result().StatusCode, 202)
+
+	// prefix
+	m = Default()
+	router, ok = m.New("host", group.NewHosts("localhost"))
+	a.True(ok).NotNil(router)
+	p := router.Prefix("/prefix1")
+	p.Get("/p1", buildHandler(203))
+	w = httptest.NewRecorder()
+	r = httptest.NewRequest(http.MethodGet, "/prefix1/p1", nil)
+	m.ServeHTTP(w, r)
+	a.Equal(w.Result().StatusCode, 404)
+
+	w = httptest.NewRecorder()
+	r = httptest.NewRequest(http.MethodGet, "http://localhost:88/prefix1/p1", nil)
+	m.ServeHTTP(w, r)
+	a.Equal(w.Result().StatusCode, 203)
+
+	// prefix prefix
+	m = Default()
+	router, ok = m.New("host", group.NewHosts("localhost"))
+	a.True(ok).NotNil(router)
+	p1 := router.Prefix("/prefix1")
+	p2 := p1.Prefix("/prefix2")
+	p2.GetFunc("/p2", buildFunc(204))
+	w = httptest.NewRecorder()
+	r = httptest.NewRequest(http.MethodGet, "/prefix1/prefix2/p2", nil)
+	m.ServeHTTP(w, r)
+	a.Equal(w.Result().StatusCode, 404)
+
+	w = httptest.NewRecorder()
+	r = httptest.NewRequest(http.MethodGet, "http://localhost/prefix1/prefix2/p2", nil)
+	m.ServeHTTP(w, r)
+	a.Equal(w.Result().StatusCode, 204)
+
+	// 第二个 Prefix 为域名
+	m = Default()
+	p1 = m.Prefix("/prefix1")
+	p2 = p1.Prefix("example.com")
+	p2.GetFunc("/p2", buildFunc(205))
+	w = httptest.NewRecorder()
+	r = httptest.NewRequest(http.MethodGet, "/prefix1example.com/p2", nil)
+	m.ServeHTTP(w, r)
+	a.Equal(w.Result().StatusCode, 205)
+}
+
 func TestMethods(t *testing.T) {
 	a := assert.New(t)
 	a.Equal(Methods(), handlers.Methods)
