418e96fdcc43bac1a0aaa1233c81412aabb7c2af
caixw <xnotepad@gmail.com>
2021-05-09 16:39:40 +0800

    feat: 添加 ApplyMiddlewares

--- middleware.go
+++ middleware.go
@@ -7,22 +7,37 @@ import "net/http"
 // MiddlewareFunc 将一个 http.Handler 封装成另一个 http.Handler
 type MiddlewareFunc func(http.Handler) http.Handler
 
+// ApplyMiddlewares 按顺序将所有的中间件应用于 h
+func ApplyMiddlewares(h http.Handler, f ...MiddlewareFunc) http.Handler {
+	if l := len(f); l > 0 {
+		for i := l - 1; i >= 0; i-- {
+			h = f[i](h)
+		}
+	}
+	return h
+}
+
+// ApplyMiddlewaresFunc 按顺序将所有的中间件应用于 h
+func ApplyMiddlewaresFunc(h func(w http.ResponseWriter, r *http.Request), f ...MiddlewareFunc) http.Handler {
+	return ApplyMiddlewares(http.HandlerFunc(h), f...)
+}
+
 // Append 添加中间件到尾部
-func (mux *Mux) Append(m MiddlewareFunc) *Mux {
+func (mux *Mux) Append(f MiddlewareFunc) *Mux {
 	if mux.middlewares == nil {
 		mux.middlewares = make([]MiddlewareFunc, 0, 5)
 	}
-	mux.middlewares = append(mux.middlewares, m)
+	mux.middlewares = append(mux.middlewares, f)
 	return mux.buildMiddlewares()
 }
 
 // Prepend 添加中间件到顶部
-func (mux *Mux) Prepend(m MiddlewareFunc) *Mux {
+func (mux *Mux) Prepend(f MiddlewareFunc) *Mux {
 	// NOTE: 当允许传递多个参数时，不同用户对添加顺序理解可能会不一样：
 	// - 按顺序一次性添加到顶部；
 	// - 单个元素依次添加到顶部；
 	ms := make([]MiddlewareFunc, 0, 1+len(mux.middlewares))
-	ms = append(ms, m)
+	ms = append(ms, f)
 	if len(mux.middlewares) > 0 {
 		ms = append(ms, mux.middlewares...)
 	}
@@ -37,14 +52,7 @@ func (mux *Mux) Reset() {
 }
 
 func (mux *Mux) buildMiddlewares() *Mux {
-	mux.handler = http.HandlerFunc(mux.serveHTTP)
-
-	if l := len(mux.middlewares); l > 0 {
-		for i := l - 1; i >= 0; i-- {
-			mux.handler = mux.middlewares[i](mux.handler)
-		}
-	}
-
+	mux.handler = ApplyMiddlewaresFunc(mux.serveHTTP, mux.middlewares...)
 	return mux
 }
 
