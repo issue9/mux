bd124231db0faebe316991c6d3deb222e67650ae
caixw <xnotepad@gmail.com>
2021-11-26 14:25:38 +0800

    feat: 添加 FileServer 用于静态文件服务

---
+++ filesystem.go
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+
+package mux
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+)
+
+type fileServer struct {
+	http.FileSystem
+	paramName    string
+	index        string
+	errorHandler func(w http.ResponseWriter, status int, msg interface{})
+}
+
+// FileServer 声明静态文件服务
+//
+// fsys 访问的文件系统；
+// name 路径保存在 context 中的参数名；
+// index 当用户访问的是目录时，将自动读取此目录下的 index 文件，如果为空则为 index.html；
+// errHandler 对各类出错的处理，如果为空会调用 http.Error 进行相应的处理。
+// 如果要自定义，目前 status 可能的值有 403、404 和 500；
+//
+//  r := NewRouter("")
+//  r.Get("/assets/{path}", FileServer(http.Dir("./assets"), "path", "index.html", nil)
+func FileServer(fsys http.FileSystem, name, index string, errHandler func(w http.ResponseWriter, status int, msg interface{})) http.Handler {
+	if fsys == nil {
+		panic("参数 fsys 不能为空")
+	}
+	if name == "" {
+		panic("参数 name 不能为空")
+	}
+
+	if index == "" {
+		index = "index.html"
+	}
+
+	if errHandler == nil {
+		errHandler = func(w http.ResponseWriter, status int, msg interface{}) {
+			http.Error(w, fmt.Sprint(msg), status)
+		}
+	}
+
+	return &fileServer{
+		FileSystem:   fsys,
+		paramName:    name,
+		index:        index,
+		errorHandler: errHandler,
+	}
+}
+
+func (f *fileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	name, found := GetParams(r).Get(f.paramName)
+	if !found {
+		panic(fmt.Sprintf("未找到参数 %s 对应的值", name))
+	}
+
+	err := f.serve(name, w, r)
+	switch {
+	case errors.Is(err, os.ErrPermission):
+		f.errorHandler(w, http.StatusForbidden, nil)
+	case errors.Is(err, os.ErrNotExist):
+		f.errorHandler(w, http.StatusNotFound, nil)
+	case err != nil:
+		f.errorHandler(w, http.StatusInternalServerError, err)
+	}
+}
+
+// p 表示文件地址，用户应该保证 p 的正确性；
+// 如果 p 是目录，则会自动读 p 目录下的 f.index 文件，
+func (f *fileServer) serve(p string, w http.ResponseWriter, r *http.Request) error {
+	if p == "" {
+		p = "."
+	}
+
+STAT:
+	fi, err := f.Open(p)
+	if err != nil {
+		return err
+	}
+
+	stat, err := fi.Stat()
+	if err != nil {
+		return err
+	}
+	if stat.IsDir() {
+		p = path.Join(p, f.index)
+		goto STAT
+	}
+
+	data, err := ioutil.ReadAll(fi)
+	if err != nil {
+		return err
+	}
+	buf := bytes.NewReader(data)
+
+	http.ServeContent(w, r, filepath.Base(p), stat.ModTime(), buf)
+	return nil
+}
---
+++ filesystem_test.go
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+package mux
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/issue9/assert/v2"
+	"github.com/issue9/assert/v2/rest"
+)
+
+func TestFileServer(t *testing.T) {
+	a := assert.New(t, false)
+	r := NewRouter("fs")
+	a.NotNil(r)
+
+	fs := FileServer(http.Dir("./"), "path", "go.mod", nil)
+	a.NotNil(fs)
+	r.Get("/assets/{path}", fs)
+
+	s := rest.NewServer(a, r, nil)
+
+	// index == go.mod
+	s.Get("/assets/").Do(nil).Status(200).
+		Status(http.StatusOK).
+		BodyFunc(func(a *assert.Assertion, body []byte) {
+			a.Contains(string(body), "module github.com/issue9/mux/")
+		})
+
+	s.Get("/assets/params/params.go").Do(nil).
+		Status(http.StatusOK).
+		BodyFunc(func(a *assert.Assertion, body []byte) {
+			a.Contains(string(body), "package params")
+		})
+
+	s.Get("/assets/not-exists").Do(nil).Status(http.StatusNotFound)
+}
